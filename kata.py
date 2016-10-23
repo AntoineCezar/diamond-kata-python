9f0d7a2d4be9e760453de1959033aca90727d62a
Antoine Cezar <antoine@cezar.fr>
2016-10-23 22:31:17 +0200

    refactor

--- kata.py
+++ kata.py
@@ -1,5 +1,8 @@
+def tail(string):
+    return string[1:]
+
 def revert(string):
     return string[::-1]
 
 def simetrize(string):
-    return string + revert(string)[1:]
+    return string + tail(revert(string))
